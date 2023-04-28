d39b61558133a9b18c69ef1784c5df8e72cf8fe1
Yaroslav <yroslav070411@gmail.com>
2023-04-29 02:00:25 +0300

    Added navlinks

--- src/components/UI/NavBar/NavBar.tsx
+++ src/components/UI/NavBar/NavBar.tsx
@@ -1,5 +1,27 @@
 import styles from './NavBar.module.scss';
+import { NavLink } from 'react-router-dom';
 
 export const Subheader = () => {
-  return <nav className={styles.nav}></nav>;
+  return (
+    <nav className={styles.nav}>
+      <div className='container'>
+        <div className={styles.wrapper}>
+          <ul className={styles.navLeft}>
+            <li className={styles.navItem}>
+              <NavLink to='/'>
+                <div className='hamburger'>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+                <span>Activity</span>
+              </NavLink>
+            </li>
+            <li className={styles.navItem}>2</li>
+            <li className={styles.navItem}>3</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
